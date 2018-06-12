d7dce2ef1a4b122e700594202c73a6f4178c8168
Ramet <Ramet.i3gateway@gmail.com>
2018-06-12 17:32:18 +0700

    home.blade edit 12062561
    edit home.blede
    chang path blade
    
    Co-Authored-By: rameti3gateway <rameti3gateway@users.noreply.github.com>

--- resources/views/Trainee.blade.php
+++ resources/views/Trainee.blade.php
@@ -1,18 +1,18 @@
-
+ @extends('layouts.app')
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Laravel</title>
 
-     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-        <!-- Styles -->
-        <style>
+    <!-- Styles -->
+    <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
@@ -63,28 +63,88 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
-        </style>
-    </head>
-    <body>
-       
-           
-          </div>
-        </nav>
-       <div class="flex-center position-ref full-height">
+</style>
+</head>
+<body>
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+        <div class="top-right links">
+            @if (Auth::check())
+            <a href="{{ url('/home') }}">Home-->></a>
+            @else
+            <a href="{{ url('/login') }}">Login</a>
+            <a href="{{ url('/register') }}">Register</a>
+            @endif
+        </div>
+        @endif
+
+        <div class="content">
+            <div class="title m-b-md">
+                 System Trainee 
+         </div>
+
+         <div class="links">
+            @if (Auth::check())
+            <a href="https://laravel.com/docs">Profile</a>
+            <a href="https://laracasts.com">Checkin-Checkout</a>
+            <a href="https://laravel-news.com">To do list</a>
+            @else
+            <a href="https://github.com/laravel/laravel">GitHub</a>
+            @endif
+        </div>
+    </div>
+</div> 
+</body>
+</html>
+
+
+
+ {{-- 
+ @extends('layouts.app')
+   <style>
+           .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+
+            .title {
+                font-size: 60px;
+            }
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 20px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+            .m-b-md {
+                margin-top: 5%;
+                margin-bottom: 40px;
+            }
+   </style>
+@section('content')
+        <div class="container"  >
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}"></a>
                     @else
                         <a href="{{ url('/login') }}">Login</a>
                         <a href="{{ url('/register') }}">Register</a>
                     @endif
                 </div>
             @endif
-
             <div class="content">
                 <div class="title m-b-md">
-                   System Trainee 
+                   <a>Welcome System Trainee</a>
                 </div>
 
                 <div class="links">
@@ -95,11 +155,9 @@
                     @else
                         <a href="https://github.com/laravel/laravel">GitHub</a>
                     @endif
-                    
-                    
-                   
                 </div>
             </div>
         </div> 
-    </body>
-</html>
+     </div>
+@endsection
+ --}}
